18cd051df83e6dbdee2b7477cc5dedebbaa5029d
Alednic <cindela06@gmail.com>
2026-02-04 13:27:30 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000105419_Cindela/week01/gameStore.kt
+++ src/oop_00000105419_Cindela/week01/gameStore.kt
@@ -1,12 +1,21 @@
 package oop_00000105419_Cindela.week01
 
 fun main(){
-    val gameTitle: String = "SteamKW"
+    val gameTitle: String = "Black Myth Wukong"
     val price: Int = 550000
+    val discountedPrice = calculateDiscount(price)
+    printReceipt(title=gameTitle, finalPrice=discountedPrice)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) {
     price * 80/100
 } else {
     price * 90/100
+}
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("-Struk Pembelian-")
+    println("Judul Game: $title")
+    println("Total Bayar: $finalPrice")
+    println()
 }
