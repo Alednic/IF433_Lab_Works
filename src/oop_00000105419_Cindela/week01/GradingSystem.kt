84031ba32410e342d64a8c21dc7b70fe889945d7
Alednic <cindela06@gmail.com>
2026-02-04 10:50:48 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000105419_Cindela/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000105419_Cindela.week01
+
+fun main(){
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    //concatenation gaya yang lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
