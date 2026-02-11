b18e2dc8c531d2ac92cfed161c8d8b874ef1d443
Alednic <cindela06@gmail.com>
2026-02-11 23:11:56 +0700

    week01: (task 1) create Loan class with default args

---
+++ src/oop_00000105419_Cindela/week02/Loan.kt
@@ -0,0 +1,13 @@
+package oop_00000105419_Cindela.week02
+
+class Loan (
+        val bookTitle : String,
+        val borrower : String,
+        var loanDuration : Int = 1
+        ){
+    fun calculateFine(): Int{
+        return if (loanDuration > 3) (loanDuration - 3) * 2000 else 0
+    }
+}
+
+
