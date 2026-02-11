b3f99705d909afdfb52d8499e736ec7c5e9a4c16
Alednic <cindela06@gmail.com>
2026-02-11 23:22:17 +0700

    week02: (task 1) implement library input system

--- src/oop_00000105419_Cindela/week02/Loan.kt
+++ src/oop_00000105419_Cindela/week02/Loan.kt
@@ -1,13 +1,34 @@
 package oop_00000105419_Cindela.week02
 
+import java.util.Scanner
+
 class Loan (
         val bookTitle : String,
         val borrower : String,
         var loanDuration : Int = 1
-        ){
+){
     fun calculateFine(): Int{
         return if (loanDuration > 3) (loanDuration - 3) * 2000 else 0
     }
 }
 
+fun main(){
+    val scanner = Scanner(System.`in`)
+    print("Book title: ")
+    val bookTitle = scanner.nextLine()
+    print("Name: ")
+    val borrower = scanner.nextLine()
+    print("Duration: ")
+    var loanDuration = scanner.nextInt()
+
+    if (loanDuration < 0){
+        loanDuration = 1
+    }
+
+    val loan = Loan(bookTitle, borrower, loanDuration)
+    println("Book Title: ${loan.bookTitle}")
+    println("Peminjam : ${loan.borrower}")
+    println("Durasi Peminjaman : ${loan.loanDuration}")
+    println("Total Denda: Rp ${loan.calculateFine()}")
+}
 
