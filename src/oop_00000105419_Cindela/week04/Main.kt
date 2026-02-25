74ef28101b788c462340df415d04d7d785964a07
Alednic <cindela06@gmail.com>
2026-02-25 10:03:03 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000105419_Cindela/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000105419_Cindela.week04
+
+fun main() {
+    println("Testing Vehicle")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\nTesting Car")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
