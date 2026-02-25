2eb3bb17d0caaf311e582a70f716c9df269eaaf5
Alednic <cindela06@gmail.com>
2026-02-25 10:13:18 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_00000105419_Cindela/week04/Employee.kt
@@ -0,0 +1,11 @@
+package oop_00000105419_Cindela.week04
+
+open class Employee(name: String, baseSalary: Int) {
+    open fun work(){
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int = (baseSalary * 0.1).toInt()
+
+    }
+}
