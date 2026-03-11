16ae9662e375a1bdb69311d214593bdb59c4caf1
Alednic <cindela06@gmail.com>
2026-03-11 10:19:41 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000105419_Cindela/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000105419_Cindela.week06
+
+interface Camera {
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
