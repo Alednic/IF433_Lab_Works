2295074587cec7af0952de56ff9d3aa8ccfe1827
Alednic <cindela06@gmail.com>
2026-03-11 10:44:41 +0700

    week06: (task) create SmartSpeaker

--- src/oop_00000105419_Cindela/week06/SmartLamp.kt
+++ src/oop_00000105419_Cindela/week06/SmartLamp.kt
@@ -4,3 +4,11 @@ class SmartLamp(override val id: String, override val name: String) : SmartDevic
     override fun turnon() { println("$name Lampu dinyalakan.") }
     override fun turnoff() { println("$name Lampu dimatikan.") }
 }
+
+class SmartSpeaker(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnon() { println("[$name] Speaker menyala.") }
+    override fun turnoff() { println("[$name] Speaker mati.") }
+    fun playMusic(song: String) {
+        println("Memutar lagu $song dari Spotify.")
+    }
+}
