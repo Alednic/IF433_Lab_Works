1b966ee9929367ce5ade4e45b4d8b290395d612a
Alednic <cindela06@gmail.com>
2026-03-25 10:03:26 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_00000105419_Cindela/week07/Main.kt
+++ src/oop_00000105419_Cindela/week07/Main.kt
@@ -1,7 +1,11 @@
 package oop_00000105419_Cindela.week07
 
-package oop_00000105419_Cindela.week07
-
 fun main() {
-    val client = NetworkClient("https://api.umn.ac.id")
+    println("TEST SINGLETON")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\nTEST COMPANION OBJECT")
+    val client = NetworkClient.createClient()
+    client.connect()
 }
