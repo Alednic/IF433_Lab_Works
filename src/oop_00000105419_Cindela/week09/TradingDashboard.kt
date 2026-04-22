31ec03f3e24df7363bbe033e853b70dd50132725
Alednic <cindela06@gmail.com>
2026-04-22 10:42:30 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000105419_Cindela/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000105419_Cindela.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 50, 45.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 10, -12.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 20, 5.0, "OPEN"),
+        TradeLog("DOGEUSDT", "SHORT", 5, 2.5, "OPEN")
+    )
+}
