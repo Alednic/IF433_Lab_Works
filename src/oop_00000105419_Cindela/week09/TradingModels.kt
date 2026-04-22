85a358076ad5b4968a6d66e88a62552d74e740be
Alednic <cindela06@gmail.com>
2026-04-22 10:52:19 +0700

    week09: (task) test full crypto trading data pipeline

--- src/oop_00000105419_Cindela/week09/TradingModels.kt
+++ src/oop_00000105419_Cindela/week09/TradingModels.kt
@@ -1,3 +1,3 @@
 package oop_00000105419_Cindela.week09
 
-data class TradeLog(val pair:String, val position: String, val leverage: Int, val roe: Double, val status: String))
+data class TradeLog(val pair:String, val position: String, val leverage: Int, val roe: Double, val status: String)
