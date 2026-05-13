3485af4fb9bdd9bc013044112f4985e0c5ba26a5
Alednic <cindela06@gmail.com>
2026-05-13 09:15:41 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000105419_Cindela/week12/SmartFeeder.kt
+++ src/oop_00000105419_Cindela/week12/SmartFeeder.kt
@@ -17,4 +17,18 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    try {
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+    } catch (e: DispenserJamException) {
+        println("Error Dispenser: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Error Stok: ${e.message}")
+    } catch (e: Exception) {
+        println("Error General: Terjadi kesalahan sistem.")
+    }
 }
